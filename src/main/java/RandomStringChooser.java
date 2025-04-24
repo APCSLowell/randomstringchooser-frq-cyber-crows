0d88f2b1082709192fc1913ce0f68bfe052faa30
cybercrow <malcolmbarrykao@gmail.com>
2025-04-23 17:24:44 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,8 +1,23 @@
 import java.util.*;
+private String [] ss; 
+private int valuesRemaining; 
 public class RandomStringChooser
 {
-  /* to be implemented in part (a) */
+  RandomStringChooser(String [] s){
+    
+for(int i=0;i<s.length;i++){
+ss[i]=s[i];
+}
+valuesRemaining=s.length; 
+  }
+public string getNext(){
+if(valuesRemaining==0)
+  return "NONE";
+int random= (int) (Math.random() * valuesRemaining);
+String selected= ss[random];
+values[random]= values[valuesRemaining-1]; //need to understand this line of code 
+valuesRemaining--; 
+  return selected; 
   
-  //Heads up! 
-  //You will get a very confusing error message until you have working code in part b as well
+}
 }
